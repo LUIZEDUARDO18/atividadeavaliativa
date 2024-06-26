9f729627d1ddfc4d020e4b5ab1ad527d688cdd1c
LUIZEDUARDO18 <130081267+LUIZEDUARDO18@users.noreply.github.com>
2024-06-26 10:53:08 -0300

    .

--- script.js
+++ script.js
@@ -47,5 +47,13 @@ let perguntaAtual;
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraPergunta();
+}
+function mostraPergunta() {
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativa.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
 }
 mostraPergunta()
